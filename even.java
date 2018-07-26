964141e9038663aff2a016520a0e62c01ac3a220
balaji1214 <30371404+balaji1214@users.noreply.github.com>
2018-07-26 16:04:13 +0530

    Update even.java

--- even.java
+++ even.java
@@ -7,7 +7,7 @@ public static void main(String[] args)
 Scanner sc=new Scanner(System.in);
 int a;
 a=nextInt();
-if(a/2)
+if(a%2==0)
 System.out.println("it is even");
 else
 System.out.println("it is odd");
