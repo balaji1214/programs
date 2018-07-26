fbd779f11d69a05240eb843e8fa194feb4bf65a1
balaji1214 <30371404+balaji1214@users.noreply.github.com>
2018-07-26 15:06:45 +0530

    Update Positive.java

--- Positive.java
+++ Positive.java
@@ -1,15 +1,11 @@
-/* package whatever; // don't place package name! */
-
 import java.util.*;
-import java.lang.*;
-import java.io.*;
 
-/* Name of the class has to be "Main" only if the class is public. */
-class Postive
+
+class Positive
 {
-	public static void main (String[] args) throws java.lang.Exception
+	public static void main (String[] args)
 	{
-		// your code goes here
+		
 		int a;
 		Scanner s=new Scanner(System.in);
 		a=s.nextInt();
