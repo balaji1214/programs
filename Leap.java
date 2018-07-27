76e3426dc9735722ce85f097897d6088a2b61673
balaji1214 <30371404+balaji1214@users.noreply.github.com>
2018-07-27 12:50:18 +0530

    Update Leap.java

--- Leap.java
+++ Leap.java
@@ -6,7 +6,6 @@ public static void main(String args[])
 {
 int year;
     	Scanner scan = new Scanner(System.in);
-    	System.out.println("Enter any Year:");
     	year = scan.nextInt();
     	scan.close();
         boolean isLeap = false;
