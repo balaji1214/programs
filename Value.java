516f396e72c4662195e0272c6022de19856b0753
balaji1214 <30371404+balaji1214@users.noreply.github.com>
2018-07-27 16:04:13 +0530

    Create Value.java

---
+++ Value.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.util.Scanner;
+public class Value
+{
+public static void main(String args[])
+{
+int count=0;
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+while(n>0)
+{
+n=n/10;
+count++;
+}
+System.out.println(count);
+}
+}
+
+{
+public static void main(String args[])
+{
+int count=0;
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+while(n>0)
+{
+n=n/10;
+count++;
+}
+System.out.println(count);
+}
+}
