dab9f2825070ea9332f8c345ed937e48ff0dfd12
balaji1214 <30371404+balaji1214@users.noreply.github.com>
2018-07-27 16:06:34 +0530

    Update Value.java

--- Value.java
+++ Value.java
@@ -16,17 +16,3 @@ System.out.println(count);
 }
 }
 
-{
-public static void main(String args[])
-{
-int count=0;
-Scanner s=new Scanner(System.in);
-int n=s.nextInt();
-while(n>0)
-{
-n=n/10;
-count++;
-}
-System.out.println(count);
-}
-}
