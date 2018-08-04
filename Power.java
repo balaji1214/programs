0e21bc6401bcae0691a4fea8b9a75ede69c78630
balaji1214 <30371404+balaji1214@users.noreply.github.com>
2018-08-04 15:51:56 +0530

    Create Power.java

---
+++ Power.java
@@ -0,0 +1,19 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+
+		Scanner sc=new Scanner(System.in);
+		int a,b;
+		a=sc.nextInt();
+		b=sc.nextInt();
+		System.out.println(Math.pow(a,b));
+	}
+}
