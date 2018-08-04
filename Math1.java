c2d0555cefd156146e57bab3b9da980f74ca16e0
balaji1214 <30371404+balaji1214@users.noreply.github.com>
2018-08-04 15:40:03 +0530

    Update and rename Math.java to Math1.java

---
+++ Math1.java
@@ -0,0 +1,19 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Math1
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int a,b;
+		a=sc.nextInt();
+		b=sc.nextInt();
+		System.out.println(Math.pow(a,b));
+	}
+}
