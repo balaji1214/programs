8f01e7ad1623c3dc2de96bd389ef0f14e0c7513f
balaji1214 <30371404+balaji1214@users.noreply.github.com>
2018-08-04 15:49:47 +0530

    Update and rename Pow.java to Power.java

--- Power.java
+++ Power.java
@@ -1,15 +1,15 @@
-/* package whatever; // don't place package name! */
+
 
 import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-/* Name of the class has to be "Main" only if the class is public. */
-class Pow
+
+class Power
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
+		
 		Scanner sc=new Scanner(System.in);
 		int a,b;
 		a=sc.nextInt();
