8492b47bb735f238ed17863a1221ae85e6f48a9d
balaji1214 <30371404+balaji1214@users.noreply.github.com>
2018-08-04 15:38:24 +0530

    Create Math.java

---
+++ Math.java
@@ -0,0 +1,19 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Math
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int a,b;
+		a=sc.nextInt();
+		b=sc.nextInt();
+		System.out.println(Math.pow(a,b));
+	}
+}
