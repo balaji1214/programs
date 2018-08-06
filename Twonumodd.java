5c3b35d724d3a6cd70a92669b07f05026c48282b
balaji1214 <30371404+balaji1214@users.noreply.github.com>
2018-08-06 15:43:17 +0530

    Create Twonumodd.java

---
+++ Twonumodd.java
@@ -0,0 +1,30 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Twonumodd
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int n1=sc.nextInt();
+		
+		for(n=1;n<=n1;n++)
+		{
+		
+		if(n%2==0)
+		{
+		//	System.out.print(+n +" ");
+			
+		}
+		else
+		System.out.print(+n +" ");
+		
+		}
+	}
+}
