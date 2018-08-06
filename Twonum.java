3efc6a105f174e1acdbae84b7534baa6960046a2
balaji1214 <30371404+balaji1214@users.noreply.github.com>
2018-08-06 15:37:29 +0530

    Create Twonum.java

---
+++ Twonum.java
@@ -0,0 +1,28 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Twonum
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int n1=sc.nextInt();
+		
+		for(n=1;n<=n1;n++)
+		{
+		
+		if(n%2==0)
+		{
+			System.out.print(+n +" ");
+			
+		}
+		
+		}
+	}
+}
