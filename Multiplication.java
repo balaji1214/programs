f3f1a3fcbecda74774a9db8fe59ff220308ce30f
balaji1214 <30371404+balaji1214@users.noreply.github.com>
2018-08-08 11:31:23 +0530

    Create Multiplication.java

---
+++ Multiplication.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Multiplication
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+		int n;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		for(int i=1;i<=n;i++)
+		System.out.println(+n*i);
+	}
+}
