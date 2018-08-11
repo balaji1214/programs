4aad5ba1d4f5f9eaa7461ab1859574d0adf418d5
balaji1214 <30371404+balaji1214@users.noreply.github.com>
2018-08-11 11:09:24 +0530

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -1,9 +1,7 @@
 /* package whatever; // don't place package name! */
-
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-
 /* Name of the class has to be "Main" only if the class is public. */
 class Palindrome
 {
@@ -14,18 +12,15 @@ class Palindrome
 Scanner sc=new Scanner(System.in);
 number=sc.nextInt();
         originalNumber = number;
-
         while (originalNumber != 0)
         {
             remainder = originalNumber % 10;
             result += Math.pow(remainder, 3);
             originalNumber /= 10;
         }
-
         if(result == number)
             System.out.println(" yes");
         else
             System.out.println(" no");
-
 	}
 }
