e50bb79420d4b789fcd3fe9e048627da3141443f
balaji1214 <30371404+balaji1214@users.noreply.github.com>
2018-08-11 13:55:50 +0530

    Create Setthree.java

---
+++ Setthree.java
@@ -0,0 +1,28 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Setthree
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int n1=sc.nextInt();
+		
+		int n2=sc.nextInt();
+		int n3=sc.nextInt();
+		
+		int n4=n-n2;
+		
+		int n5=n1-n3;
+		int n6=Math.abs(n4);
+		int n7=Math.abs(n5);
+		System.out.println(+ n6 + " " + n7);
+		
+	}
+}
