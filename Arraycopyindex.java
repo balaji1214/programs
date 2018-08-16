2ab3b70e56e5cfd117546dba4a7a79c316cdcdd7
balaji1214 <30371404+balaji1214@users.noreply.github.com>
2018-08-16 10:23:54 +0530

    Create Arraycopyindex.java

---
+++ Arraycopyindex.java
@@ -0,0 +1,32 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Arraycopyindex
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int[] arr=new int[100000];
+		int n=sc.nextInt();
+		for(int i=0;i<n;i++)
+		{
+			arr[i]=sc.nextInt();
+		
+		
+		
+	
+	
+		}
+		for(int i=0;i<n;i++)
+		{
+			System.out.println(arr[i] +" " +i);
+		}
+		
+	}
+	
+}
