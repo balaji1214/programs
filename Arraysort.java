5f62ad24c7da2108641befd3ac106a4523032cc5
balaji1214 <30371404+balaji1214@users.noreply.github.com>
2018-08-16 11:46:14 +0530

    Update Arraysort.java

--- Arraysort.java
+++ Arraysort.java
@@ -10,50 +10,27 @@ class Copied
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
-		Scanner sc=new Scanner(System.in);
-		int[] arr=new int[100000];
-		int n=sc.nextInt();
+		
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		a[i]=s.nextInt();
+		
 		for(int i=0;i<n;i++)
 		{
-		  arr[i]=sc.nextInt();
-	
+			for(int j=0;j<n;j++)
+			{
+				if(a[i]<a[j])
+				{
+					int temp=a[i];
+					a[i]=a[j];
+					a[j]=temp;
+				}
+				
+			}
 		}
-		        for (int i = 0; i < n; i++) 
-
-        {
-
-            for (int j = i + 1; j < n; j++) 
-
-            {
-
-                if (a[i] > a[j]) 
-
-                {
-
-                    temp = a[i];
-
-                    a[i] = a[j];
-
-                    a[j] = temp;
-
-                }
-
-            }
-
-        }
-
-       
-
-        for (int i = 0; i < n - 1; i++) 
-
-        {
-
-            System.out.print(a[i] + " ");
-
-        }
-
-        System.out.print(a[n - 1]);
-		
-		
+		for(int i=0;i<n;i++)
+System.out.print(a[i]+" ");
 	}
 }
