c6ad18be3635afb6c3aa5ce5fc45989587becef0
balaji1214 <30371404+balaji1214@users.noreply.github.com>
2018-08-17 11:44:34 +0530

    Create Stringisnumornot.java

---
+++ Stringisnumornot.java
@@ -0,0 +1,32 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Stringisnumeric
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		String s=br.readLine();
+		boolean numeric = true;
+
+        try {
+            Double num = Double.parseDouble(s);
+        } catch (NumberFormatException e) {
+            numeric = false;
+        }
+
+        if(numeric)
+            System.out.println("Yes");
+  
+		
+		
+		
+		
+	}
+}
